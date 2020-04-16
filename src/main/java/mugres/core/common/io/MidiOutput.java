2a08f0af6240b4e815ecec019d2b4d5ccb5a658d
raltamirano <rodrigo.altamirano@gmail.com>
2020-04-16 08:43:27 -0300

    NOTE_OFF issue fixed

--- src/main/java/mugres/core/common/io/MidiOutput.java
+++ src/main/java/mugres/core/common/io/MidiOutput.java
@@ -19,7 +19,8 @@ public class MidiOutput implements Output {
     public void send(final Signal signal) {
         try {
             final ShortMessage message = new ShortMessage(NOTE_ON, signal.getChannel(),
-                    signal.getPlayed().getPitch().getMidi(), signal.getPlayed().getVelocity());
+                    signal.getPlayed().getPitch().getMidi(),
+                    signal.isActive() ? signal.getPlayed().getVelocity() : 0);
             midiOutputPort.send(message, -1);
         } catch (final InvalidMidiDataException e) {
             throw new RuntimeException(e);
