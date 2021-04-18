c0c1d8e7b593dce3415103e76ca028ca0d9feef6
raltamirano <rodrigo.altamirano@gmail.com>
2021-04-18 14:48:21 -0300

    Add special "remaining" range to tag all the signals that didn't get tagged by other ranges

--- src/main/java/mugres/core/filter/builtin/misc/Ranges.java
+++ src/main/java/mugres/core/filter/builtin/misc/Ranges.java
@@ -30,11 +30,19 @@ public class Ranges extends Filter {
         final List<Range> ranges = getRanges(arguments);
 
         for(final Signal in : signals.signals()) {
-            for(int index=0; index<ranges.size(); index++) {
-                if (ranges.get(index).contains(in.getPlayed().getPitch())) {
-                    in.addTag(ranges.get(index).getTag());
+            boolean matched = false;
+            for(final Range range : ranges) {
+                if (!range.isRemaining() && range.contains(in.getPlayed().getPitch())) {
+                    matched = true;
+                    in.addTag(range.getTag());
                     break;
                 }
+            }
+
+            if (!matched) {
+                for(final Range range : ranges)
+                    if (range.isRemaining())
+                        in.addTag(range.getTag());
             }
         }
 
@@ -55,9 +63,15 @@ public class Ranges extends Filter {
             if (matcher.matches()) {
                 final String[] parts = rangeValue.split(PARTS_SEPARATOR);
                 final String tag = parts[0];
-                final int start = Integer.valueOf(parts[1]);
-                final int end = Integer.valueOf(parts[2]);
-                final Range range = Range.of(tag, Pitch.of(start), Pitch.of(end));
+                final boolean remaining = REMAINING_MARK.equals(parts[1]);
+                final Range range;
+                if (remaining) {
+                    range = Range.remaining(tag);
+                } else {
+                    final int start = Integer.valueOf(parts[1]);
+                    final int end = Integer.valueOf(parts[2]);
+                    range = Range.of(tag, Pitch.of(start), Pitch.of(end));
+                }
                 validateNoOverlapping(ranges, range);
                 ranges.add(range);
             }
@@ -69,6 +83,9 @@ public class Ranges extends Filter {
     }
 
     private void validateNoOverlapping(final List<Range> ranges, final Range toCheck) {
+        if (toCheck.isRemaining())
+            return;
+
         for(final Range existing : ranges)
             if (existing.overlapsWith(toCheck))
                 throw new RuntimeException("Ranges filter can allow overlapping ranges!");
@@ -76,7 +93,8 @@ public class Ranges extends Filter {
 
     private static final String RANGES_SEPARATOR = ",";
     private static final String PARTS_SEPARATOR = ":";
-    private static final Pattern RANGE_PATTERN = Pattern.compile("[a-zA-Z0-9\\/\\_\\-]+" + PARTS_SEPARATOR + "\\d{1,3}+\\" + PARTS_SEPARATOR + "\\d{1,3}");
+    private static final String REMAINING_MARK = "*";
+    private static final Pattern RANGE_PATTERN = Pattern.compile("[a-zA-Z0-9\\/\\_\\-]+" + PARTS_SEPARATOR + "(\\d{1,3}+\\" + PARTS_SEPARATOR + "\\d{1,3}|\\" + REMAINING_MARK +")");
     private static final Pattern RANGES_PATTERN = Pattern.compile("^" + RANGE_PATTERN.pattern() + "(" + RANGES_SEPARATOR + RANGE_PATTERN.pattern() + ")*$");
 
     private static class Range implements Comparable<Range> {
@@ -91,7 +109,20 @@ public class Ranges extends Filter {
         }
 
         public static Range of(final String tag, final Pitch start, final Pitch end) {
+            if (tag == null || tag.trim().isEmpty())
+                throw new IllegalArgumentException("tag");
+            if (start == null)
+                throw new IllegalArgumentException("start");
+            if (end == null)
+                throw new IllegalArgumentException("end");
+            if (start.compareTo(end) > 0)
+                throw new IllegalArgumentException("start can't be greater than end!");
+
             return new Range(tag, start, end);
+        }
+
+        public static Range remaining(final String tag) {
+            return new Range(tag, null, null);
         }
 
         public String getTag() {
@@ -114,8 +145,14 @@ public class Ranges extends Filter {
             return pitch.compareTo(start) >= 0 && pitch.compareTo(end) <= 0;
         }
 
+        public boolean isRemaining() {
+            return start == null && end == null;
+        }
+
         @Override
         public int compareTo(final Range o) {
+            if (isRemaining()) return 1;
+            else if (o.isRemaining()) return -1;
             return Integer.compare(this.start.getMidi(), o.start.getMidi());
         }
     }
