e82b33b6b8e0b4209a6fbf665186048c11872a08
raltamirano <rodrigo.altamirano@gmail.com>
2021-04-25 12:32:48 -0300

    Improved signal handling

--- src/main/java/mugres/core/live/processor/transformer/config/Configuration.java
+++ src/main/java/mugres/core/live/processor/transformer/config/Configuration.java
@@ -3,10 +3,7 @@ package mugres.core.live.processor.transformer.config;
 import mugres.core.filter.Filter;
 import mugres.core.live.signaler.Signaler;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static java.util.Collections.unmodifiableList;
 
@@ -26,8 +23,80 @@ public class Configuration {
         signalers.add(signaler);
     }
 
+    public void appendFilter(final String filter, final String parameter1, final Object value1) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        appendFilter(filter, parameters);
+    }
+
+    public void appendFilter(final String filter, final String parameter1, final Object value1,
+                             final String parameter2, final Object value2) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        parameters.put(parameter2, value2);
+        appendFilter(filter, parameters);
+    }
+
+    public void appendFilter(final String filter, final String parameter1, final Object value1,
+                             final String parameter2, final Object value2,
+                             final String parameter3, final Object value3) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        parameters.put(parameter2, value2);
+        parameters.put(parameter3, value3);
+        appendFilter(filter, parameters);
+    }
+
+    public void appendFilter(final String filter, final String parameter1, final Object value1,
+                             final String parameter2, final Object value2,
+                             final String parameter3, final Object value3,
+                             final String parameter4, final Object value4) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        parameters.put(parameter2, value2);
+        parameters.put(parameter3, value3);
+        parameters.put(parameter4, value4);
+        appendFilter(filter, parameters);
+    }
+
     public void appendFilter(final String filter, final Map<String, Object> args) {
         filters.add(new FilterEntry(Filter.forName(filter), args));
+    }
+
+    public void appendFilter(final Filter filter, final String parameter1, final Object value1) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        appendFilter(filter, parameters);
+    }
+
+    public void appendFilter(final Filter filter, final String parameter1, final Object value1,
+                             final String parameter2, final Object value2) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        parameters.put(parameter2, value2);
+        appendFilter(filter, parameters);
+    }
+
+    public void appendFilter(final Filter filter, final String parameter1, final Object value1,
+                             final String parameter2, final Object value2,
+                             final String parameter3, final Object value3) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        parameters.put(parameter2, value2);
+        parameters.put(parameter3, value3);
+        appendFilter(filter, parameters);
+    }
+
+    public void appendFilter(final Filter filter, final String parameter1, final Object value1,
+                             final String parameter2, final Object value2,
+                             final String parameter3, final Object value3,
+                             final String parameter4, final Object value4) {
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put(parameter1, value1);
+        parameters.put(parameter2, value2);
+        parameters.put(parameter3, value3);
+        parameters.put(parameter4, value4);
+        appendFilter(filter, parameters);
     }
 
     public void appendFilter(final Filter filter, final Map<String, Object> args) {
