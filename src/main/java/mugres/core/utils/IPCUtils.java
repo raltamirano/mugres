dc8acc06684750542f92e609a8a20441ca8b8018
raltamirano <rodrigo.altamirano@gmail.com>
2021-06-19 14:39:24 -0300

    Improve IPC utils

--- src/main/java/mugres/core/utils/IPCUtils.java
+++ src/main/java/mugres/core/utils/IPCUtils.java
@@ -4,6 +4,8 @@ import mugres.ipc.Listener;
 import mugres.ipc.tcpip.MUGRESTCPIPClient;
 import mugres.ipc.tcpip.MUGRESTCPIPServer;
 
+import java.util.function.Consumer;
+
 import static mugres.ipc.tcpip.Utils.getMUGRESServerPortOrDefault;
 
 public class IPCUtils {
@@ -14,11 +16,26 @@ public class IPCUtils {
      *
      * @param label short description of the name or purpose of this server (for logging and stuff like that)
      * @param args Command line arguments (for configuration)
-     * @param clientMessageProcessor Listener for messages sent from clients to this server
+     * @param onServerStarted Server started callback
      */
     public static void runMUGRESTCPIPServer(final String label,
                                             final String[] args,
-                                            final Listener clientMessageProcessor) {
+                                            final Consumer<MUGRESTCPIPServer> onServerStarted) {
+        runMUGRESTCPIPServer(label, args, null, onServerStarted);
+    }
+
+    /**
+     * Start a MUGRES TCP/IP server
+     *
+     * @param label short description of the name or purpose of this server (for logging and stuff like that)
+     * @param args Command line arguments (for configuration)
+     * @param clientMessageProcessor Listener for messages sent from clients to this server
+     * @param onServerStarted Server started callback
+     */
+    public static void runMUGRESTCPIPServer(final String label,
+                                            final String[] args,
+                                            final Listener clientMessageProcessor,
+                                            final Consumer<MUGRESTCPIPServer> onServerStarted) {
         try {
             final int port = getMUGRESServerPortOrDefault(System.getProperty("mugres.server.listener-port"));
             System.out.println(String.format("Starting MUGRES TCP/IP server %s on port %d ...", label, port));
@@ -27,6 +44,8 @@ public class IPCUtils {
             mugresTCPIPServer.setListener(clientMessageProcessor);
             mugresTCPIPServer.start();
             System.out.println(String.format("MUGRES TCP/IP server %s started", label));
+
+            onServerStarted.accept(mugresTCPIPServer);
 
             Thread.currentThread().join();
             mugresTCPIPServer.stop();
@@ -39,10 +58,23 @@ public class IPCUtils {
      * Start a MUGRES TCP/IP client
      *
      * @param args Command line arguments (for configuration)
-     * @param clientMessageProcessor Listener for messages sent from clients to this node
+     * @param onClientConnected Client connected callback
      */
     public static void runMUGRESTCPIPClient(final String[] args,
-                                            final Listener clientMessageProcessor) {
+                                            final Consumer<MUGRESTCPIPClient> onClientConnected) {
+        runMUGRESTCPIPClient(args, null, onClientConnected);
+    }
+
+    /**
+     * Start a MUGRES TCP/IP client
+     *
+     * @param args Command line arguments (for configuration)
+     * @param clientMessageProcessor Listener for messages sent from clients to this node
+     * @param onClientConnected Client connected callback
+     */
+    public static void runMUGRESTCPIPClient(final String[] args,
+                                            final Listener clientMessageProcessor,
+                                            final Consumer<MUGRESTCPIPClient> onClientConnected) {
         try {
             final String host = args[0];
             final int port = Integer.parseInt(args[1]);
@@ -52,6 +84,8 @@ public class IPCUtils {
             mugresTCPIPClient.setListener(clientMessageProcessor);
             mugresTCPIPClient.connect();
             System.out.println("MUGRES TCP/IP client connected");
+
+            onClientConnected.accept(mugresTCPIPClient);
 
             Thread.currentThread().join();
             mugresTCPIPClient.disconnect();
